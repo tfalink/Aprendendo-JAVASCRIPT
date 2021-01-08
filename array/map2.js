d987eeb8a31e3d33b0c859fb7b9e097804f4765c
tfalink <tempeskron.gr@gmail.com>
2021-01-08 10:04:36 -0400

    adicionado map2.js

---
+++ array/map2.js
@@ -0,0 +1,11 @@
+const carrinho = [
+    '{ "nome": "Borracha", "preco": 3.45 }',
+    '{ "nome": "Caderno", "preco": 13.90 }',
+    '{ "nome": "Kit de lápis", "preco": 41.22 }',
+    '{ "nome": "Caneta", "preco": 7.50 }'
+]
+
+const json = e => JSON.parse(e)
+const apenasPreco = produto => produto.preco
+const resultado = carrinho.map(json).map(apenasPreco)
+console.log(resultado)
