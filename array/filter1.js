7aa980a04361a07c161b0e4de6ea11c817cdb2f3
tfalink <tempeskron.gr@gmail.com>
2021-01-08 10:44:24 -0400

    adicionado filter1.js

---
+++ array/filter1.js
@@ -0,0 +1,14 @@
+const produtos = [
+    { nome: 'Notebook', preco: 2499, fragil: true },
+    { nome: 'iPad Pro', preco: 4199, fragil: true },
+    { nome: 'Copo de Vidro', preco: 12.49, fragil: true },
+    { nome: 'Copo de Plástico', preco: 18.99, fragil: false }
+]
+
+console.log(produtos.filter(function(p) {
+    return false
+}))
+
+const prodFragil = e => e.fragil
+const prodCaro = e => e.preco >= 500
+console.log(produtos.filter(prodFragil).filter(prodCaro))
