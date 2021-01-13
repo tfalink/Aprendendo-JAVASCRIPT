fe66219fdff4ca343f15727eb20d0265ce22c2de
tfalink <tempeskron.gr@gmail.com>
2021-01-12 21:36:54 -0400

    Primeiras aulas de node

--- exercicios_fundamentos/exercicio30.js
+++ exercicios_fundamentos/exercicio30.js
@@ -15,4 +15,4 @@ console.log(receberMelhorEstudante({
     Joao: [8, 7.6, 8.9, 6], // média 7.625
     Mariana: [9, 6.6, 7.9, 8], // média 7.875
     Carla: [7, 7, 8, 9] // média 7.75
-    })) // retornará { nome: "Mariana", media: 7.875
+})) // retornará { nome: "Mariana", media: 7.875
